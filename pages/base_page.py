a370456e06ba1a8d796f28027c14bfb20d2512dd
NiK_B <alacrate@gmail.com>
2021-03-08 17:48:44 +0800

    Добавлен тест наличия ссылки для регистрации

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage:
     def __init__(self, browser, url, timeout=5) -> None:
         self.browser = browser
-        self.browser.implicity_wait(timeout)
+        self.browser.implicitly_wait(timeout)
         self.url = url
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, by, element):
+        try:
+            self.browser.find_element(by, element)
+        except NoSuchElementException:
+            return False
+        return True

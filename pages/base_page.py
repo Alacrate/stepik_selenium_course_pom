9f4bbc6ae802da1d98bfe27757601f9da38ba9be
NiK_B <alacrate@gmail.com>
2021-03-08 17:39:39 +0800

    Задание неявного таймаута перенесено из conftest в base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,7 @@
 class BasePage:
-    def __init__(self, browser, url) -> None:
+    def __init__(self, browser, url, timeout=5) -> None:
         self.browser = browser
+        self.browser.implicity_wait(timeout)
         self.url = url
 
     def open(self):
